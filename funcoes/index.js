e152f21fcb88953e2abf598d5f153363d00deaf7
thiagobacelar <thiago.bacelar7@gmail.com>
2021-03-18 11:41:36 -0300

    melhorias em funções imediatas

--- funcoes/index.js
+++ funcoes/index.js
@@ -2,25 +2,30 @@
 var resultado;
 
 // funções normais
-function nome(nome){
-    console.log(nome);
+function nome(){
+    console.log('thiago');
 }
-nome('thiago');
+nome();
 
+//funções com argumentos
+
+function somarNumeros(){
+    let soma =0;
+    console.log(arguments);
+    //arguments => em caso de não ser passado parametro
+    for(let argumento of arguments){
+        soma += argumento;
+    }
+    console.log("soma é:" + soma);
+}
+somarNumeros(1,5,10,7,23,50);
 
 
 
 //funções imediatas
 (function(nomecompleto){
-   
-    if(nomecompleto == '' || nomecompleto == null){
-        alert('informe um nome !');
-    }else{
-        console.log(nomecompleto);
-    }
-
+   console.log(nomecompleto);
 })('thiago henrique');
-
 
 
 
@@ -30,9 +35,7 @@ numeroAlatorio(10,20, imrimir =>{
   resultado = imrimir;
   console.log(`o numero sorteado é: ${resultado}`);
 })
-
 function numeroAlatorio(max,min, callback){
-     
     let aleatorio = Math.random() * (max - min) + min;
     callback(Math.floor(aleatorio));
     return aleatorio;
